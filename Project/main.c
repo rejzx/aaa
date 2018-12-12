626fdabcffd286a6171c7dbb51406c8abf9e6732
rejzx <rich.gehrer@gmail.com>
2018-12-12 18:56:13 +0100

    Main Function

---
+++ Project/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(){
+
+    while (true) {
+        int q = getchar();
+        if (q == 'k') {break;}
+        switch(q)
+        {
+        case 'n':
+
+            break;
+
+        case 'v':
+
+            break;
+
+        case 'p':
+
+            break;
+
+        case 'z':
+
+            break;
+
+        case 'h':
+
+            break;
+
+        case 'a':
+
+            break;
+
+
+        }
+
+    }
+
+    return 0;
+}
