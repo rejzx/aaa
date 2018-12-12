970fca3f0785f528ee50947e108ec1827773b98c
rejzx <rich.gehrer@gmail.com>
2018-12-12 19:15:53 +0100

    Added all funcions

--- Project/main.c
+++ Project/main.c
@@ -4,7 +4,54 @@
 #include <string.h>
 #include <ctype.h>
 
+typedef struct carNode {
+
+    char  category [50];
+    char  mark     [50];
+    char  seller  [100];
+    long   prize;
+    long   dateOfMade;
+    char  actualCondition[200];
+    struct carNode *next;
+} carNode;
+
+typedef struct linkedList {
+
+    carNode* head;
+    carNode* tail;
+    int size;
+
+} linkedList;
+
+void init(){}
+
+carNode* fillNode(){return 0;}
+
+carNode* insertedNode(){return 0;}
+
+void insertAt(){}
+
+void n(){}
+
+void v(){}
+
+void fC(){}
+
+void zN(){}
+
+void z(){}
+
+void p(){}
+
+void h(){}
+
+void a(){}
+
+int k(){}
+
 int main(){
+    FILE *pFile = NULL ;
+    linkedList list;
 
     while (true) {
         int q = getchar();
@@ -12,33 +59,34 @@ int main(){
         switch(q)
         {
         case 'n':
-
+            n();
             break;
 
         case 'v':
-
+            v();
             break;
 
         case 'p':
-
+            p();
             break;
 
         case 'z':
-
+            z();
             break;
 
         case 'h':
-
+            h();
             break;
 
         case 'a':
-
+            a();
             break;
 
 
         }
 
     }
-
+    k();
     return 0;
 }
+
